6d4db7764027e772c96873798ae09e4effd2a9d6
Berihuete Nunez, Engel <mastering23@gmail.com>
2025-02-06 13:08:56 -0500

    practices api set up

--- myapp-user-page/src/App.jsx
+++ myapp-user-page/src/App.jsx
@@ -2,24 +2,63 @@ import React, { useState, useEffect } from "react";
 
 const App = () => {
   const [allUsers, setAllUsers] = useState([]);
+  const [emailInput, setEmailInput] = useState([]);
+  const [nameInput, setName] = useState([]);
+  const [passwordInput, setPassword] = useState([]);
+  const [roleInput, setRole] = useState([]);
+  const [avatarInput, setAvatar] = useState([]);
 
   useEffect(() => {
     const getUsers = async () => {
-      const response = await fetch(`https://api.escuelajs.co/api/v1/users`);
+      const response = await fetch(`https://api.escuelajs.co/api/v1/users`);//get request
       const retrievedUsers = await response.json();
       setAllUsers(retrievedUsers);
     };
     getUsers();
   }, []);
 
+  const CreateUser = async (event) => {  
+    event.preventDefault();
+
+    const response = await fetch('https://api.escuelajs.co/api/v1/users', {  
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            email: emailInput,
+            name: nameInput,
+            password: passwordInput,
+            role: roleInput,
+            avatar: avatarInput
+        })
+    });
+
+    const x = await response.json();
+    console.log(x);
+   
+  }
+
   return (
     <>
       <h1>TESTING</h1>
+      <ul>
+      <form onSubmit={CreateUser}>
+        <input placeholder="email" type="email" onChange={(event)=>{setEmailInput(event.target.value)}} />
+        <input placeholder="name"  onChange={(event)=>{setName(event.target.value)}} />
+        <input placeholder="password"   type ="password" onChange={(event)=>{setPassword(event.target.value)}} />
+        <input placeholder="role" onChange={(event)=>{setRole(event.target.value)}} />
+        <input placeholder="avatar" onChange={(event)=>{setAvatar(event.target.value)}} />
+        <button>Submit</button>
+      </form>
+      </ul>
       <ul>
         {allUsers.map((singleUser) => (
           <li key={singleUser.id}>{singleUser.name}</li>
         ))}
       </ul>
+
+      
     </>
   );
 };
